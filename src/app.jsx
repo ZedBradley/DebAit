77f4d85c667adb2c73dd56de0d83abd53a10baa5
Zed Bradley <zedbradley30@gmail.com>
2025-11-17 16:56:29 +0000

    react test

--- src/app.jsx
+++ src/app.jsx
@@ -1,24 +1,4 @@
 // src/App.jsx
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import StartDebate from "./pages/StartDebate";
-import Explore from "./pages/Explore";
-import About from "./pages/About";
-
-function App() {
-  return (
-    <div className="page">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/start" element={<StartDebate />} />
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </div>
-  );
+export default function App() {
+  return <h1>DebAIt TEST</h1>;
 }
-
-export default App;
